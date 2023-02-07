e1b57d76402cc7f6de38f83bf3a69a2ada6f1a70
Kornovalx <vladislav.konowalov2012@yandex.ru>
2023-02-08 00:36:46 +0300

    Idea Test

---
+++ src/ru/inordic/java092022/lesson7/Main.java
@@ -0,0 +1,7 @@
+package ru.inordic.java092022.lesson7;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/ru/inordic/java092022/lesson7/Tt.java
@@ -0,0 +1,4 @@
+package ru.inordic.java092022.lesson7;
+
+public class Tt {
+}
